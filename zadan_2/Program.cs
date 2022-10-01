67045f68869181eda12b285c32596349473850ac
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-02 01:29:44 +0300

    z2-выполнено

---
+++ zadan_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+string userInput = Console.ReadLine() ?? "";
+double userNumber_a = double.Parse(userInput);
+Console.Write("Введите второе число: ");
+userInput = Console.ReadLine() ?? "";
+double userNumber_b = double.Parse(userInput);
+Console.Write("Введите третье число: ");
+userInput = Console.ReadLine() ?? "";
+double userNumber_c = double.Parse(userInput);
+double max = userNumber_a;
+if (userNumber_b > max) max = userNumber_b;
+if (userNumber_c > max) max = userNumber_c;
+{
+    Console.WriteLine(userNumber_a + ", " + userNumber_b + ", " + userNumber_c + " -> " + max);
+}
