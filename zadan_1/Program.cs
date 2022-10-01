fac19c36d2db12febe68a3f07045bf1566b190d7
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-01 21:57:55 +0300

    z1-выполнено

---
+++ zadan_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число a: ");
+string userInput = Console.ReadLine() ?? "";
+double userNumber_a = double.Parse(userInput);
+Console.Write("Введите число b: ");
+userInput= Console.ReadLine() ?? "";
+double userNumber_b = double.Parse(userInput);
+if (userNumber_a > userNumber_b)
+{
+    Console.WriteLine("a=" + userNumber_a + " b=" + userNumber_b + " ->" + " max=" + userNumber_a);
+}
+else
+{
+    Console.WriteLine("a=" + userNumber_a + " b=" + userNumber_b + " ->" + " max=" + userNumber_b);
+}
