40f8123f186e40ea18a1f98fc51ce025938bcc6a
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-07 19:20:14 +0300

    3

---
+++ zadan_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string b = Console.ReadLine() ?? "";
+int a = int.Parse(b);
+
+if (a %2 == 0)
+{
+    Console.WriteLine(a + " -> " + "да");
+}
+else
+{
+    Console.WriteLine(a + " -> " + "нет");
+}
